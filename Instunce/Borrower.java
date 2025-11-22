55cddbcddb321113465b0c9bbb5a35b9e0ce2e14
nishi <jjhqp1110@gmail.com>
2025-11-22 20:58:26 +0900

    Book과 Borrower 작업 완료했습니다

--- Instunce/Borrower.java
+++ Instunce/Borrower.java
@@ -1,5 +1,6 @@
 package Instunce;
 
+import java.util.*;
 
 /**
  * Borrower 클래스의 설명을 작성하세요.
@@ -7,29 +8,33 @@ package Instunce;
  * @author (작성자 이름)
  * @version (버전 번호 또는 작성한 날짜)
  */
-public class Borrower
-{
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
-
-    /**
-     * Borrower 클래스의 객체 생성자
-     */
-    public Borrower()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
+public class Borrower{
+    private int borrowerId;
+    private String name = null;
+    
+    private ArrayList<Loan> loans = null;
+    
+    public Borrower(int borrowerId, String name){
+        this.borrowerId = borrowerId;
+        this.name = name;
+        
+        this.loans = new ArrayList<Loan>();
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+    
+    public int getBorrowerId(){ return this.borrowerId; }
+    
+    public boolean loanAbleCheck(){
+        return this.loans.size() < 10;
+    }
+    
+    public void connectLoan(Loan loan){
+        this.loans.add(loan);
+    }
+    public void disconnectLoan(Loan loan){
+        this.loans.remove(loan);
+    }
+    
+    public String toString(){
+        return this.borrowerId + " " + this.name;
     }
 }
